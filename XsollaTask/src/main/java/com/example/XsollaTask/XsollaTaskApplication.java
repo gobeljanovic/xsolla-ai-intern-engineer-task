6c349d6fe574957ad4b1b73ccb506f3055a5fade
djole <djordjenrt4023@gs.viser.edu.rs>
2026-08-21 11:02:35 +0200

    initial commit

---
+++ XsollaTask/src/main/java/com/example/XsollaTask/XsollaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.XsollaTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsollaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsollaTaskApplication.class, args);
+	}
+
+}
---
+++ XsollaTask/src/test/java/com/example/XsollaTask/XsollaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.XsollaTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsollaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
